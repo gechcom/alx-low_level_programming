6f1201b1474e8cf64b61bb36e8b6708b7d8cb40c
Getinet Ayenew <getinetay@gmail.com>
2022-06-02 13:55:35 -0700

    size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 
 printf("Size of an double: %lu byte(s)\n", sizeof(double));
 
-rintf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 
