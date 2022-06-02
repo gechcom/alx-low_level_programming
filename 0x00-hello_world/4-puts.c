ed195a1d943f41f0ad59fc118457142b8e731230
Getinet Ayenew <getinetay@gmail.com>
2022-06-02 12:47:06 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main function where program execution starts
+ * main - Entry point
  *
- * C program that prints with put function
+ * Description: A C program that prints with put function
  *
- * Return 0 all the time
+ * Return: Always 0 (Success)
  */
+ 
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
