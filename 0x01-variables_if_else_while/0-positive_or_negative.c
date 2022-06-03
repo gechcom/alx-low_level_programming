19e07a395d455d68f4bada1abe02ac302fef4eaa
Getinet Ayenew <getinetay@gmail.com>
2022-06-03 02:23:59 -0700

    variable n status

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,16 @@
 int main(void)
 {
 int n;
+
 srand(time(0));
-n=rand()-RAND_MAX/2;
+n = rand() - RAND_MAX /2;
 
 if(n>0)
 	printf("%i is positive\n", n);
 
 if(n==0)
 	printf("%i is zero\n", n);
-else
+else(n<0)
 	printf("%i is negative\n", n);
 return (0);
 }
