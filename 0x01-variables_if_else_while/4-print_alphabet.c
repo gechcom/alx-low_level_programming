fb61db1b8ebe5c8d1b8338e9734340f55a8a04ef
Getinet Ayenew <getinetay@gmail.com>
2022-06-03 05:02:57 -0700

    no q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print the the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'a';
+char CH = 'A';
+while (ch <= 'z')
+{
+	if (ch != e || ch != q)
+	putchar(ch);
+	else
+	++ch;
+}
+putchar('\n');
+
+return (0);
+}
