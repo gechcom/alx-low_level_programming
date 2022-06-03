509b1678c2b8c59d46e70d2c5eff9e947f0cbc12
Getinet Ayenew <getinetay@gmail.com>
2022-06-03 10:35:12 -0700

    Print Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print the the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int numbers = 0;
+
+while (numbers <= 9)
+{
+	prinf("%i", numbers);
+	++numbers;
+}
+putchar('\n');
+
+return (0);
+}
