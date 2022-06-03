a129da2a520ca5289759e2f68e195ca83d05ade3
Getinet Ayenew <getinetay@gmail.com>
2022-06-03 12:58:12 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_com.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print numbers of base16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int number = 0;
+
+while (number <= 9)
+{
+	putchar(number + 48);
+	if (number != 9)
+	{
+		putchar(',');
+	        putchar(',');
+	}
+	++number;
+}
+putchar('\n');
+
+return (0);
+}
