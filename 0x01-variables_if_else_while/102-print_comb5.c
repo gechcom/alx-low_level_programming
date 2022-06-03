a6cda8d3dc78af39286c268de5231c963d144f41
Getinet Ayenew <getinetay@gmail.com>
2022-06-03 15:51:04 -0700

    combination of 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,15 +18,15 @@ while (digit1 <= 99)
 	while (digit2 <= 99)
 
 	{
-		if (digit1 != digit2 )
+		if (digit1 != digit2)
 		{
-		putchar((digit1 / 10 ) + 48);
+		putchar((digit1 / 10) + 48);
 		putchar((digit1 % 10) + 48);
 		putchar(' ');
 		putchar((digit2 / 10) + 48);
 		putchar((digit2 % 10) + 48);
 
-		if (digit1 != 98 || digit2 !=99)
+		if (digit1 != 98 || digit2 != 99)
 		{
 			putchar(',');
 			putchar(' ');
