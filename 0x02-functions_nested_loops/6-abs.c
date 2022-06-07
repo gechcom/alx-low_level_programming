8ec669f0a0c39ce4b5ca2528a566884a806bf290
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 15:37:15 -0700

    Absolute value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs: compute absolute valueof int
+ *
+ * @n: takesint data taye
+ *
+ * Return: Always 0
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1)*n;
+	return (n);
+}
