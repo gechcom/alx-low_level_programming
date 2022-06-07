b270a0f9c5d8f71cb9d31bdca046de332f9c1828
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 12:27:45 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
 #include"main.h"
+
 /**
  * main- Entry point
  *
- * print alphabet
+ * Description: print alphabet
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+
+void print_alphabet(void) 
 {
 	int c;
+
 	for (c = 'a'; c <= 'z'; ++c)
 		_putchar(c);
+
 	_putchar('\n');
 }
