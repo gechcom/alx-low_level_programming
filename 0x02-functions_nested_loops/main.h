258a8112c391292af69d4a301c82e4b651504d87
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 14:04:01 -0700

    Check Lower and UPpercaser

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int);
-int _isaplha(int);
+int _isaplha(int c);
 int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
