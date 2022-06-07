c85c7d4cba08f8d8bd1b0e7b22e24a6e00f13798
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 13:27:05 -0700

    10 times Alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alhabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int line, c;
+
+	for (line = 0; line <= 9; ++line )
+	{
+		for (c = 'a'; c <= 'z'; ++c)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
