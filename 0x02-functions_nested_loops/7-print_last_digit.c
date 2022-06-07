f0981205fe279e887bbc7285bb2e2b1a66979327
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 15:50:02 -0700

    Print last digit number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_lasy_digit - print th elast digit num
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	if (n < 0)
+		l = -1 * (n % 10);
+	else
+		l = n % 10;
+	return (n);
+}
