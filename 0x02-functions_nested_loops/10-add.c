57e53a7b415973b71cccf88892e7101f99cb6e65
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 16:49:54 -0700

    Sum numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add numbers
+ *
+ * @x: takes first number
+ * @y: takes second number
+ *
+ * Return:sum
+ */
+
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
