a6349292f0b3171648db91ee531e2c0a821b00c1
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 14:38:29 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print +, 0, -
+ * 
+ * @n: takes interger
+ *
+ * Return: 1,  0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48 + 0);
+		return (0);
+	}
+	else
+	{
+		putchar(45 + 0);
+		return(-1);
+	}
+}
