dd30e05802508272b8f64eac016e5e63b1300cf8
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 17:40:27 -0700

    sum of multiple of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ *Descpription: cum of all the multiplie of 3  0r 5 below 1024
+ *
+ *Return: Always 0 
+ */
+int main(void)
+{
+	int x, sum;
+
+	for (x = 0; x <= 1024; ++x)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+			sum += x;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+
+}
