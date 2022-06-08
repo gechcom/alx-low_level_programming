a22dba6cf8a84402a09351ed9955584466a3898a
Getinet Ayenew <getinetay@gmail.com>
2022-06-07 18:10:37 -0700

    Fabonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ *@num: operand number
+ *
+ *Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+
+}
