3c5a0547e55550caf098a37f9a6ca9713340535f
Getinet Ayenew <getinetay@gmail.com>
2022-06-08 09:43:50 -0700

    Largest Number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,17 +5,19 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ *
  * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	if (a > b && a > c)
+
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
@@ -24,5 +26,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return (largest)'
+	return (largest);
 }
