106a571892c6552c1b96823abe0eb43ee62498e1
Getinet Ayenew <getinetay@gmail.com>
2022-06-09 13:35:45 -0700

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - checks if input is digit between 0 - 9
+ *
+ * @a: input one
+ * @b: input two
+ *
+ * Return: results
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
