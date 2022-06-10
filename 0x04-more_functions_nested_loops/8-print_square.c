078dd243433c057b64e17a74f2bbf02c49dd58ad
Getinet Ayenew <getinetay@gmail.com>
2022-06-09 22:30:27 -0700

    Print Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,11 +12,11 @@ void print_square(int size)
 
 	/* if (size <= 0)*/
 		/*_putchar('\n');*/
-		for (c = 2; c <= size; ++c)
+		for (c = 1; c <= size; ++c)
 		{
 			/*_putchar('#');*/
 
-			for (r = 2; r <= size; ++r)
+			for (r = 1; r <= size; ++r)
 				_putchar('#');
 			_putchar('\n');
 		}
