e7b2626074f90681ebcc00faa14750fd683986e2
Getinet Ayenew <getinetay@gmail.com>
2022-06-09 22:32:39 -0700

    Print Square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_square - prints a square
  * 
@@ -6,16 +7,13 @@
  *
  * Return: value
  */
+
 void print_square(int size)
 {
 	int c, r;
 
-	/* if (size <= 0)*/
-		/*_putchar('\n');*/
 		for (c = 1; c <= size; ++c)
 		{
-			/*_putchar('#');*/
-
 			for (r = 1; r <= size; ++r)
 				_putchar('#');
 			_putchar('\n');
