8a451271d6c190e8de7062a7ea7d3b012d975857
Getinet Ayenew <getinetay@gmail.com>
2022-06-13 06:43:35 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - calcualte the lenth of the string
+ * @s: string
+ * Return: the lenthe of the string
+ */
+
+int _strlen(char *s);
+{
+	int len;
+
+	len = strlen(*s);
+
+	return (len);
+}
