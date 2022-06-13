5a56f7039d11b4147ef984cbfa0daa7fc46aa459
Getinet Ayenew <getinetay@gmail.com>
2022-06-13 14:16:46 -0700

    copy pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy one ponted values to theother
+ * @dest: parameter one
+ * @src: parameter two
+ * Return : the string copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}
