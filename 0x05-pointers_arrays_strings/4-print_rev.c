478a59d1339c40303cadfc6285af067adf73d3ec
Getinet Ayenew <getinetay@gmail.com>
2022-06-13 12:09:58 -0700

    reverse print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,13 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * print_rev - print string in reverse order
+ * @s: string parameter
+ * Return: NOthing
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
