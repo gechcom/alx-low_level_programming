bb86f985bc436fb1f932b41dd97665081fba51e1
Getinet Ayenew <getinetay@gmail.com>
2022-06-13 05:21:06 -0700

    change to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the ponter valueto 98
+ * @*n: pointer parameter
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
