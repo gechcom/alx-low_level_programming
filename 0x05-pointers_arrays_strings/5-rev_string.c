e8496d7bdfc1aef097eab1739b6d8c29ca0375be
Getinet Ayenew <getinetay@gmail.com>
2022-06-13 12:43:45 -0700

    reverse print

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function to reverse a sting
+ * @s: string parameter
+ * return Nothing
+ */
+
+void rev_string(char *s)
+{
+	strrev(s);
+}
