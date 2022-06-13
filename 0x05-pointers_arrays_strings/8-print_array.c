5493c34538bf38d2247d5cd6965301d85a5ad545
Getinet Ayenew <getinetay@gmail.com>
2022-06-13 14:04:01 -0700

    array print

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - function to print aray
+ * @a: paramter a string
+ * @i: string parameter two
+ * Return: noting
+ */
+
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
