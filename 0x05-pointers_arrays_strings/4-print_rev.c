e586a6c82f798a7d1abf3fdf942ffb62b4fb79ec
Getinet Ayenew <getinetay@gmail.com>
2022-06-13 11:58:50 -0700

    reverse print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,16 @@
 
 void print_rev(char *s)
 {
-	int i, len, temp;
+	int i, len;
 
 	len = strlen(s);
 
-	for (i = 0; i <= len/2; i++)
+	for (i = len; i >=0 ; --i)
 	{
-		temp = s[i];
-		s[i] = s[len - i -1];
-		s[len - i - 1] = temp;
+		_putchar(s[i]);
+	
 	}
 
-	puts(s);
+	_putchar('\n);
 
 }
