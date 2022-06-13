3be69cc378001cdabb4823bdb72b2b8c0d24b6af
Getinet Ayenew <getinetay@gmail.com>
2022-06-13 12:39:07 -0700

    reverse print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,6 +16,6 @@ void print_rev(char *s)
 	while (s[index] != '\0')
 		index++;
 	for (--index; index >= 0; --index)
-		_putchar(s[index]);
-	_putchar('\n');
+		putchar(s[index]);
+	putchar('\n');
 }
