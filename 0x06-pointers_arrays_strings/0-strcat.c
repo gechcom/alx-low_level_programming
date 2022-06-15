a2af52a4f0b8619d3aa358da836379bbc8d3bf12
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 11:35:43 -0700

    String concatinate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * _strcat - this function overwriting the terminating null byute (\0) at the end of the dest
  * @dest:string parameter
  * @src: another string parameter
- * Return:returning a pointer to the resulting string dest
+ * Return: returning a pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src);
