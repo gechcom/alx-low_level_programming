1339618ff5b5df4eca177edb036a5eb657c070f9
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 14:35:28 -0700

    uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,16 +3,17 @@
 /**
  * string_toupper - change to to upper case function
  * @s: string needs to be changed to upperrcase
- * Return: uppercase string
+ * Return: @s
  */
 
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (s[i] != '\0')
 	{
+		/*Compare charactre based on thier ASCII Code*/
+
 		if(s[i] >= 97 && s[i] <= 122)
 			s[i] = s[i] - 32;
 		i++;
