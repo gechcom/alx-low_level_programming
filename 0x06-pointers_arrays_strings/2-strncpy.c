cdc6be9b96aadea3658b74d56cce54055a6e7caf
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 12:29:34 -0700

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copying strings
+ * @dest: pointer to destination
+ * @src: pointer to source
+ * @n: byte of source pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
