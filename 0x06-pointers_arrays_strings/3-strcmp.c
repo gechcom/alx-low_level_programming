9b314348112ebe40f281cc82d3f11e5e5b774663
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 13:27:40 -0700

    string comparision

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, flag = 0;
+	int i = 0, r;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			flag = 1;
+			r = s1[i] - s2[i];
 			break;
 		}
-
+		else
+			r = s1[i] - s2[i];
 		i++;
 	}
-
-	if (flag == 0)
-		return (0);
-	else
-		return (1);
-
+	return (r);
 }
