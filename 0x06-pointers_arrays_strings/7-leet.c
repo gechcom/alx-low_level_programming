10ef5673beca91aeffb8f701c9f8ff1d2c06f113
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 15:36:45 -0700

    uppercase

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,23 +9,22 @@
 
 char *leet(char *s)
 {
-	int i, j = 0;
+	int i, c = 0;
 	int sl[] = {97, 101, 111, 116, 108};
 	int ul[] = {65, 69, 79, 84, 76};
 	int n[] = {52, 51, 48, 55, 49};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[c] != '\0')
 	{
-		for (j = 0; j < 5; j++)
+		for (i = 0; i < 5; i++)
 		{
-			if (s[i] == s1[j] || s[i] == u1[j])
+			if (s[c] == sl[i] || s[c] == ul[i])
 			{
-				s[i] = n[j];
+				s[c] = n[i];
 				break;
 			}
 		}
+		c++;
 	}
-
 	return (s);
 }
-
