35e10b5c65e7746e87b197a071d2d1f6e695c0d2
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 13:53:40 -0700

    array reversewq

---
+++ 0x06-pointers_arrays_strings/4-rev-array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverse its array
+ * @a: array
+ * @n: num of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n-i-1];
+		a[n-i-1] = temp;
+	}
+}
