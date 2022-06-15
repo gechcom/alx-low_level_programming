1dca17505650ba1f05cc36ff7514fe91f959ae11
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 12:05:13 -0700

    String concatinate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,9 +13,17 @@
 
 char *_strncat(char *dest, char *src, int n);
 {
-	char *ptr;
-	
-	ptr = strncat(dest, src, n);
+	int i, j;
 
-	return (ptr);
+	i = 0;
+
+	while (dest[i])
+		i++;
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+
+	dest[i + j] = '\0';
+
+	return (dest);
 }
