d26c61ad28585a1e94bd807792eea39633a0c99b
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 23:23:21 -0700

    Rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,34 +13,27 @@
 
 char *rot13(char *s)
 {
-	if (s == NULL)
+	if (s != NULL)
 	{
-		return NULL;
-	}
+		char* current_char = s;
 
-	char *sr = malloc(strlen(s));
 
-	if (sr != NULL)
-	{
-		strcpy(sr, s);
-		char *current_char = sr;
-
-		while ( current_char !+ '\0')
+		while ( *current_char != '\0')
 		{
-			if ((current_char >= 97 && current_char <= 122) || (current_char >= 65 && current_char <= 90))
+			if ((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90))
 			{
-				if (current_char > 109 || (current_char > 77 && current_char < 91))
+				if (*current_char > 109 || (*current_char > 77 && *current_char < 91))
 				{
-					current_char -= 13;
+					*current_char -= 13;
 				}
 				else
 				{
-					current_char += 13;
+					*current_char += 13;
 				}
 			}
 
 		current_char++;
 		}
 	}
-	return (sr);
+	return (s);
 }
