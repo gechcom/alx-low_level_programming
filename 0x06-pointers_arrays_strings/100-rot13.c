e314423ffded23496cc226a36c7b380bc87b12aa
Getinet Ayenew <getinetay@gmail.com>
2022-06-16 00:19:07 -0700

    Rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,6 +8,22 @@
 
 char *rot13(char *s)
 {
+	char *ch = s;
+
+	while (*ch != '\0')
+	{
+		if ((*ch >=97 && *ch <= 122) || (*ch >= 65 && *ch <= 90))
+		{
+			if (*ch > 109 || (*ch > 77 && *ch < 91))
+			{
+				*ch -= 13;
+			}
+			else
+				*ch += 13;
+		}
+		ch++;
+
+	}
 	return(s);
 }
 
