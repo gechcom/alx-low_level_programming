b0a1e6a0fe577689225b0ef67acbf3faae073200
Getinet Ayenew <getinetay@gmail.com>
2022-06-16 06:23:42 -0700

    magic

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,6 +8,27 @@
 
 void print_number(int n)
 {
-	_putchar(n);
+	int digit, tens, x;
+
+	digit = n;
+	tens = 1;
+
+	if (digit < 0)
+		_putchar ('_');
+	for (x = 0; digit > 9 || digit < -9; x++)
+	{
+		digit /= 10;
+		tens *= 10;
+	}
+
+	for (digit = n; x >= 0; x--)
+	{
+		if (digit / tens < 0)
+			_putchar((digit / tens) * -1 + '0');
+		else
+			_putchar ((digit / tens) + '0');
+		digit %= tens;
+		tens /= 10;
+	}
 
 }
