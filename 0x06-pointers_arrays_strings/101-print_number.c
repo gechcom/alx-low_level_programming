6e8f1098f355526f3ce76beb9a1a5e1c761296ff
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 22:31:25 -0700

    Print Number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,13 @@
+#incldue "main.h"
+
+/**
+ * print_number - write a function that prints an integer
+ * @n: integer number input
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	_putchar(n);
+
+}
