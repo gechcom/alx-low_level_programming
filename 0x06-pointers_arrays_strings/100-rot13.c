7fa4c08ddb9a9ebf704c2f5d49d75f6cc2d33338
Getinet Ayenew <getinetay@gmail.com>
2022-06-15 22:54:36 -0700

    Rot 13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,13 +1,43 @@
 #include "main.h"
 
 /**
- * rot13 - a function encoding using ro13
+ * rot13 - a function encoding using ro13 which is a function takes a string and returns the string ciphered with
+ * Rot13. if there are number or special characterstics included in the string, they are returned as they are.
+ * 
  * @s: string input
  * Return: encoded string
  */
 
 char *rot13(char *s)
 {
+	if (s == NULL)
+	{
+		return NULL;
+	}
 
-	return (s);
+	char *sr = malloc(strlen(s));
+
+	if (sr !+ NULL)
+	{
+		strcpy(sr, s);
+		char *current_char = sr;
+
+		while ( *current_char !+ '\0')
+		{
+			if ((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90))
+			{
+				if (*current_char > 109 || (*current_char > 77 && *current_char < 91))
+				{
+					*current_char -= 13;
+				}
+				else
+				{
+					*current_char += 13;
+				}
+			}
+
+		current_char++;
+		}
+	}
+	return (sr);
 }
