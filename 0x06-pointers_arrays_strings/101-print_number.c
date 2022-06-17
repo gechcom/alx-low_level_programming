1911a47a91356ef053c352e02dd0554fb5b8b3f3
gechcom <getinetay@gmail.com>
2022-06-17 05:41:03 +0300

    Update 101-print_number.c
    print num

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,4 +8,18 @@
 
 void print_number(int n)
 {
+  if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else if (n > 0)
+		n *= -1;
+	else
+		_putchar('-');
+	if ((n / 10) != 0)
+	{
+		print_number((n / 10) * -1);
+	}
+	_putchar((n % 10) * -1 + '0');
 }
