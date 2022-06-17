fa9d16833028adca8fd86d723ec87267f5f26203
gechcom <getinetay@gmail.com>
2022-06-17 08:41:54 +0300

    Update 101-print_number.c
    Print INt Number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,38 +8,16 @@
 
 void print_number(int n)
 {
-  unsigned int size = n;
-  int digits = 0;
-  int i;
+  unsigned int num = n;
 
-  if (n < 0)
-    size = -n;
-  if (n == 0)
-    digits = 1;
-  while (size >= 1)
-  {
-    size = size / 10;
-    digits++;
-  }
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-  while (i < digits)
-  {
-    int pow = 1;
-    int j;
-    int d;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-    for (j = 0; j < digits - i - 1; j++)
-      pow = pow * 10;
-
-    d = ((n / pow) % 10);
-    if (n < 0)
-    {
-      d = -d;
-      if (i == 0)
-        _putchar(45);
-    }
-    _putchar(48 + d);
-
-    i++;
-  }
+	_putchar((num % 10) + '0');
 }
