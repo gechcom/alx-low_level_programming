b5e23901a8ce191e44a4fd2449a1bbfa564188e5
gechcom <getinetay@gmail.com>
2022-06-17 05:43:14 +0300

    Update 101-print_number.c
    printijnt

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,38 @@
 
 void print_number(int n)
 {
+  unsigned int size = n;
+  int digits = 0;
+  int i;
+
+  if (n < 0)
+    size = -n;
   if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	else if (n > 0)
-		n *= -1;
-	else
-		_putchar('-');
-	if ((n / 10) != 0)
-	{
-		print_number((n / 10) * -1);
-	}
-	_putchar((n % 10) * -1 + '0');
+    digits = 1;
+  while (size >= 1)
+  {
+    size = size / 10;
+    digits++;
+  }
+
+  while (i < digits)
+  {
+    int pow = 1;
+    int j;
+    int d;
+
+    for (j = 0; j < digits - i - 1; j++)
+      pow = pow * 10;
+
+    d = ((n / pow) % 10);
+    if (n < 0)
+    {
+      d = -d;
+      if (i == 0)
+        _putchar(45);
+    }
+    _putchar(48 + d);
+
+    i++;
+  }
 }
