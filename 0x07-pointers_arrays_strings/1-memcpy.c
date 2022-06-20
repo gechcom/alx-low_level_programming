76bdb1e9d62795e0e126ea84cc346400e784b232
Getinet Ayenew <getinetay@gmail.com>
2022-06-20 14:40:52 -0700

    Memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy - functions copies n bytes from
+ * memory area src to memory area dest
+ * @dest: destination
+ * @src: source
+ * @n: integer n num of bytes
+ * @Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
