ee570aec6b3a2f2485b13988b5480d0fdb5cc70f
Getinet Ayenew <getinetay@gmail.com>
2022-06-20 14:13:15 -0700

    Memory files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memset - a unction that files memory with a constant
+ * @s: array of s
+ * @b: string b input
+ * @n: integer input
+ * Return: function files the first n bytes of the memory area pointed by s with the onstant byte b
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+
+}
