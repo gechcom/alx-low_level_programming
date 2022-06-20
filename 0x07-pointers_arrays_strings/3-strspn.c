f4c366c23ef23dd4a0866b467565416f99cd4f63
Getinet Ayenew <getinetay@gmail.com>
2022-06-20 15:22:37 -0700

    Length of prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,7 +14,8 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		flag = 1; 
+		flag = 1;
+
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
@@ -23,7 +24,7 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 		}
-		if ( flag == 1)
+		if (flag == 1)
 			break;
 	}
 
