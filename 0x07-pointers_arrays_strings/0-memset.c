c6d212575b21a444c37f17c6f7ec2b9aabc72b3d
Getinet Ayenew <getinetay@gmail.com>
2022-06-20 14:34:13 -0700

    Memory files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,8 @@
  * @s: array of s
  * @b: string b input
  * @n: integer input
- * Return: function files the first n bytes of the memory area pointed by s with the onstant byte b
+ * Return: function files the first n bytes of 
+ * the memory area pointed by s with the onstant byte b
  */
 
 char *_memset(char *s, char b, unsigned int n)
