ef86542d567fb02b0fb5deb8e8a2a33951788561
Getinet Ayenew <getinetay@gmail.com>
2022-06-22 09:15:46 -0700

    prime nummber

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _is_prime_wraper - checking prime numbers
+ * @f: factor check
+ * @p: posible prime number
+ * Return: 1 if prime, o if not
+ */
+
+int _is_prime_wraper(int f, int p)
+{
+	if (p < 2 || p % f == 0)
+		return (0);
+	else if (f > p / 2)
+		return (1);
+	else
+		return (prime_check(f + 1, p));
+}
+/**
+ * is_prime_number - check if the input number is prime number
+ * @n: Number
+ * Return: NOthing
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (_is_prime_wraper(2, n));
+}
+
