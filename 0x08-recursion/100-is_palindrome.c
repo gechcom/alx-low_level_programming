82e257c6946311056be72b5f1b44f0ecc485bd29
Getinet Ayenew <getinetay@gmail.com>
2022-06-22 10:42:59 -0700

    check palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of string
+ * @s: string input
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
+
+}
+
+/**
+ * _palindrome_check - check palindrome of a string
+ * a palindrome is when orignal and reverse string is the same
+ * @s: string
+ * @l: left index
+ * @r: rioght index
+ * Return: 1 is a palindrom o if not
+ */
+
+int _palindrome_check(char *s, int l, int r)
+{
+	if (s == NULL || l < 0 || r < 0)
+		return (1)
+	if (l >= r)
+		return (1);
+	return (_palindrome_check(s, l + 1, r - 1));
+}
+
+/**
+ * is_palindrome - status of a palindrome
+ * @s: input string
+ * Return: status
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (_palindrome_check(s, 0, _strlen(s) - 1));
+}
---
+++ 0x08-recursion/100_is_palindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_palindrome - check if a string and its reverse are the same
+ * an empty string is a palindrome
+ * @s: string 
+ * Return: 1 if a string is palindrome and 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0)
+		return (1);
+
+
+}
