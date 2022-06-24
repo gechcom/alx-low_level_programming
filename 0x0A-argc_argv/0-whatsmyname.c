ba1b10d03fce2e09e679777301d7d9c6f9c1496c
Getinet Ayenew <getinetay@gmail.com>
2022-06-24 05:15:04 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * @argc: the number of command line arguments
+ * @argv: the command line arguments
+ * Return: 0 sucess
+ */
+
+int main(int argc, int argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
