ecb56dfb9650a69bc82911f54131516f86c7f0e7
Getinet Ayenew <getinetay@gmail.com>
2022-06-27 13:38:53 -0700

    String to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,96 @@
+#include "main.h"
+
+
+
+
+/**
+ * wordcount - countngn word  form string
+ * @str: string
+ * Return: the number of string
+ */
+
+int wordcount(char *str)
+{
+	int words = 0;
+
+	while (*str != '\0')
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			while (*str != ' ' && *str != '\0')
+				str++;
+			words++;
+		}
+	}
+	return (words);
+}
+
+
+/**
+ * free_array - free array
+ * @ar: array to free
+ * @i: array[i]
+ * Return: Nothig
+ */
+void free_array(char **ar, int i)
+{
+	if (ar != NULL && i != 0)
+	{
+		while (i >= 0)
+		{
+			free(ar[i]);
+			i--;
+		}
+		free(ar);
+	}
+}
+
+
+/**
+ * strtow - a funciton that splits a string into words
+ * @str: a string
+ * Return: words
+ */
+
+char **strtow(char *str)
+{
+	int i, s, j, str_l, word;
+	char **string;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	str_l = wordcount(str);
+
+	string = malloc((str_l + 1) * sizeof(char *));
+	if (str_l == 0 || string == NULL)
+		return (NULL);
+
+	for (i = s = 0; i < str_l; i++)
+	{
+		for (word = s; str[word] != '\0'; word++)
+		{
+			if (str[word] == ' ')
+				s++;
+			if (str[word] != ' ' && (str[word + 1] == ' ' || str[word + 1] == '\0'))
+			{
+				string[i] = malloc((word - s + 2) * sizeof(char));
+				if (string[i] == NULL)
+				{
+					free_array(string, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+
+		for (j = 0; s <= word; s++, j++)
+			string[i][j] = str[s];
+		string[i][j] = '\0';
+
+	}
+	string[i][j] = '\0';
+	return (string);
+}
