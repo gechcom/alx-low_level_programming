ec8783e7aee53ecf733a81b5e3f90c3c92051ffe
gechcom <getinetay@gmail.com>
2022-06-30 21:22:10 +0300

    Update 101-mul.c
    Multiplication

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,134 +1,132 @@
 #include "main.h"
 
 /**
- * main - Multiply two numbers using algorithm taught by Laura Roudge
- * @argc: The number of command line arguments
- * @argv: The command line arguments
+ * _memset - fills memory with a constant byte
  *
- * Return: 0 if successful, 1 if program does not receive two arguments
- */
-int main(int argc, char *argv[])
-{
-	int i, length1, length2;
-	int *mul;
-
-	if (argc != 3)
-		error(98);
-
-	for (i = 1; i < argc; i++)
-		if (!(is_positive_number(argv[i])))
-			error(98);
-
-	length1 = _strlen(argv[1]);
-	length2 = _strlen(argv[2]);
-
-	mul = _calloc(length1 + length2 + 1, sizeof(int));
-	if (mul == NULL)
-		error(98);
-
-	multiply(mul, argv[1], length1, argv[2], length2);
-	free(mul);
-
-	return (0);
-}
-
-/**
- * is_positive_number - Check if a string contains only digits
- * @number: The string to check
+ * @s: input pointer that represents memory block
+ *     to fill
+ * @b: characters to fill/set
+ * @n: number of bytes to be filled
  *
- * Return: 1 if string contains only digits, 0 otherwise
- */
-int is_positive_number(char *number)
-{
-	int i;
+ * Return: pointer to the filled memory area
+*/
 
-	for (i = 0; number[i] != '\0'; i++)
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		if (!(number[i] >= '0' && number[i] <= '9'))
-			return (0);
+		s[i] = b;
+		i++;
 	}
-
-	return (1);
+	return (s);
 }
 
 /**
- * error - Print "Error" and exit
- * @status: Error number
- */
-void error(int status)
-{
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
-	exit(status);
-}
-
-
-
-/**
- * _calloc - Allocate memory for an array
- * @nmemb: The number of elements
- * @size: The number of bytes for each element
+ * _calloc - function that allocates memory
+ *           for an array using memset
  *
- * Return: Pointer to the allocated memory
- */
+ * @nmemb: size of array
+ * @size: size of each element
+ *
+ * Return: pointer to new allocated memory
+*/
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
 		return (NULL);
+	_memset(ptr, 0, nmemb * size);
 
-	_memset(p, 0, nmemb * size);
-	return (p);
+	return (ptr);
 }
 
 
-
 /**
- * multiply - Multiply two numbers and print the result
- * @mul: Pointer to store the product
- * @s1: Pointer to the first multiple
- * @length1: String length of s1
- * @s2: Pointer to the second multiple
- * @length2: String length of s2
- */
-void multiply(int *mul, char *s1, int length1, char *s2, int length2)
+ * multiply - initialize array with 0 byte
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: nothing
+*/
+
+void multiply(char *s1, char *s2)
 {
-	int i, j, l1, l2, p1, p2, c;
+	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
+	char *ptr;
+	void *temp;
 
-	l1 = length1 - 1;
-	l2 = length2 - 1;
+	l1 = _length(s1);
+	l2 = _length(s2);
+	tmp = l2;
+	total_l = l1 + l2;
+	ptr = _calloc(sizeof(int), total_l);
 
-	for (i = l1; i >= 0; i--)
+	/* store our pointer address to free later */
+	temp = ptr;
+
+	for (l1--; l1 >= 0; l1--)
 	{
-		c = 0;
-		p1 = s1[i] - '0';
-		for (j = l2; j >= 0; j--)
+		f_digit = s1[l1] - '0';
+		res = 0;
+		l2 = tmp;
+		for (l2--; l2 >= 0; l2--)
 		{
-			p2 = s2[j] - '0';
-			c += mul[i + j + 1] + (p1 * p2);
-			mul[i + j + 1] = c % 10;
-			c /= 10;
+			s_digit = s2[l2] - '0';
+			res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
+			ptr[l1 + l2 + 1] = res % 10;
+			res /= 10;
 		}
-		if (c > 0)
-			mul[i + j + 1] += c;
+		if (res)
+			ptr[l1 + l2 + 1] = res % 10;
 	}
 
-	i = 0;
-	while (mul[i] == 0)
-		i++;
-	if (i >= length1 + length2 + 1)
-		_putchar('0');
+	while (*ptr == 0)
+	{
+		ptr++;
+		total_l--;
+	}
 
-	for (; i < (length1 + length2); i++)
-		_putchar(mul[i] + '0');
-	_putchar('\n');
+	for (i = 0; i < total_l; i++)
+		printf("%i", ptr[i]);
+	printf("\n");
+	free(temp);
+}
+
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that multiplies
+ *            two positive numbers
+ *
+ * @argc: number of arguments
+ * @argv: arguments array
+ *
+ * Return: 0 on success 98 on faliure
+*/
+
+int main(int argc, char *argv[])
+{
+	char *n1 = argv[1];
+	char *n2 = argv[2];
+
+	if (argc != 3 || check_number(n1) || check_number(n2))
+		error_exit();
+
+	if (*n1 == '0' || *n2 == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+	else
+		multiply(n1, n2);
+	return (0);
 }
