8868dcc6c1357db8697f2c7cd10f0c2fb2e9dcc1
gechcom <getinetay@gmail.com>
2022-07-04 21:45:23 +0300

    Create 2-main.c
    file macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print the name of the file it was compiled from
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Statu
