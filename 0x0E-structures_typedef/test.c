36c7aade275693dd75f7933e4346ce02777641be
Getinet <getinetay@gmail.com>
2022-07-06 11:42:24 +0300

    test

--- 0x0E-structures_typedef/test.c
+++ 0x0E-structures_typedef/test.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-char *P = "Hell this is from variable"
+int main(void)
+
+char *p = {"Hello this is from variable"};
+int d = &p;
+
   printf("Hello World from atom\n");
   printf("Hello Gech  this is from desktop\n");
   printf("%s\n", p);
+  printf("%d\n", &p);
   return 0;
 }
