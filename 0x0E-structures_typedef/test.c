28f6579ca012f90a25ac25924d3d64c36d4053d5
Getinet <getinetay@gmail.com>
2022-07-06 11:07:05 +0300

    update test c

--- 0x0E-structures_typedef/test.c
+++ 0x0E-structures_typedef/test.c
@@ -1,8 +1,8 @@
-#include <stdio>
+#include <stdio.h>
 
-int int main(int argc, char const *argv[]) {
+int main(void) {
 
-  printf("Hello World from atom %s\n", );
-  printf("Hello Gech  this is from desktop\n", );
+  printf("Hello World from atom\n");
+  printf("Hello Gech  this is from desktop\n");
   return 0;
 }
