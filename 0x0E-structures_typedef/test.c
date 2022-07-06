4d52e463c98eec346f2797f7d40a783f17d5cb12
Getinet <getinetay@gmail.com>
2022-07-06 11:35:05 +0300

    test update

--- 0x0E-structures_typedef/test.c
+++ 0x0E-structures_typedef/test.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-
+char *P = "Hell this is from variable"
   printf("Hello World from atom\n");
   printf("Hello Gech  this is from desktop\n");
+  printf("%s\n", p);
   return 0;
 }
