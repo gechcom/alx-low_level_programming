3808c3214f567ced3df16f99d5551ceed7074dda
gechcom <getinetay@gmail.com>
2022-07-07 22:55:00 +0300

    Update and rename main.h to function_pointers.h
    function pointer header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,4 +9,4 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* MAIN_H */
+#endif /*FUNCTION_POINTERS_H*/
