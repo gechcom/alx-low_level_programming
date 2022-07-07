51a6a002aceb04322da3399f56b959dcf7527d25
Getinet Ayenew <getinetay@gmail.com>
2022-07-07 12:28:39 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_name - print a name
+ * @name: The name
+ * @f: pointer to the function to call
+ * Rturn: nothinig
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if ((*f) == NULL)
+		return;
+	(*f)(name);
+}
