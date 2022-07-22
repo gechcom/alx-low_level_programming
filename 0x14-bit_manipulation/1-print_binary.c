823f734702b74254a33bfed86236b8660ef5630f
gechcom <getinetay@gmail.com>
2022-07-22 22:43:48 +0300

    Create 1-print_binary.c
    Print Binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - Print the binary representation of a number
+ * @n: The number
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+	int start_printing = 0;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		for (i = 1UL << 63; i > 0; i >>= 1)
+		{
+			if (n & i)
+			{
+				_putchar('1');
+				start_printing = 1;
+			}
+			else if (start_printing)
+				_putchar('0');
+		}
+	}
+}
