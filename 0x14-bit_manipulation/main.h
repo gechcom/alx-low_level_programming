cdae6b8563e88dae150e346d765fc92197903a92
Getinet <getinetay@gmail.com>
2022-07-21 22:15:33 -0700

    Main Header

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,6 +8,9 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+int _strlen(const char *s);
+int _putchar(char c);
+unsigned int count_flipped_bits(unsigned long int n);
 
 
 #endif  /* MAIN_H */
